817dc490a3a232099360a800c5702cb3e86dfb0d
minghede267-jpg <minghede267@gmail.com>
2026-02-24 02:48:31 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: "100px", textAlign: "center" }}>
+      <h1>欢迎来到二次元角色扮演站</h1>
+      <p>项目已成功跑通！</p>
+      <a href="/login" style={{ color: "blue" }}>前往登录</a>
+    </div>
+  );
+}
